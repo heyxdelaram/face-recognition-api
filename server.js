1d4cb1877e3140447b8572f97ae3e485c2aca087
heyxdelaram <dellafarsa@gmail.com>
2023-05-24 19:31:32 +0330

    signup and login added

--- server.js
+++ server.js
@@ -56,31 +56,61 @@ app.get("/", (req, res) => {
 
 //login
 app.post("/login", (req, res) => {
-    if(req.body.email === database.users[0].email 
-        && req.body.password === database.users[0].password){
-            res.json(database.users[0])
-    }else{
-        res.json("email or password incorrect")
-    }
+    knex.select("email", "hash")
+    .from("login")
+    .where("email", "=", req.body.email)
+    .then(data => {
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
+        if(isValid){
+            knex.select("*").from("users")
+            .where("email", "=", req.body.email)
+            .then(user => res.json(user[0]))
+            .catch(err => res.status(400).json("cannot get user"))
+        }else{
+            res.status(400).json("user not found")
+        }
+    })
+    .catch(err => res.status(400).json("wrong credentials"))
 })
 
-//signup
+//signup -- chatgpt help bc mysql doesn't have a returning function so that made it difficult
 app.post("/signup", (req, res) => {
     const { email, password, username } = req.body;
-    if(email !== "" && password !== "" && username !== ""){
-        knex("users")
-        // .returning("*")
-        .insert({
-            username: username,
-            email: email,
-            date: new Date()
-        }).then(user => {
-            res.json(knex(user))
-            // res.json(knex('users').where('id', user[0]))
+    const hash = bcrypt.hashSync(password);
+    
+    if (email !== "" && password !== "" && username !== "") {
+        knex.transaction((trx) => {
+            trx("users")
+                .insert({
+                    email: email,
+                    hash: hash
+                })
+                .into("login")
+                .then(() => {
+                    return trx("users")
+                        .insert({
+                            username,
+                            email,
+                            entries: 0,
+                            date: new Date()
+                        })
+                        .then(() => {
+                            return trx("users")
+                                .select("*")
+                                .where("email", email)
+                                .then(user => {
+                                    res.json(user[0]);
+                                });
+                        });
+                })
+                .then(trx.commit)
+                .catch(trx.rollback);
         })
-        .catch(err => res.status(400).json(err))
+        .catch((err) => res.status(400).json(err));
     }
-})
+});
+
+
 
 // function findUserById(id){
 //     let user;
@@ -96,37 +126,24 @@ app.post("/signup", (req, res) => {
 //profile
 app.get("/profile/:id", (req, res)=>{
     const {id} = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id){
-            found = true;
-            return res.json(user)
+    knex.select("*").from("users").where("id", id)
+    .then(user => {
+        if(user.length){
+            res.json(user[0])
+        }else{
+            res.status(400).json("Not found")
         }
     })
-    if (!found){
-        res.status(404).json("profile not found")
-    }
-    // const user = findUserById(id)
-    // if(user){
-    //     res.json(user)
-    // }else{
-    //     res.status(404).json("profile not found")
-    // }
+    .catch(err => res.status(400).json("error getting user"))
 })
 
 
 //image
 app.put("/image", (req, res)=>{
     const {id} = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id){
-            found = true;
-            user.entries++;
-            return res.json(user.entries)
-        }
-    })
-    if (!found){
-        res.status(404).json("profile not found")
-    }
+    knex('users')
+    .where({id})
+    .increment("entries", 1)
+    .then(() => knex("users").select("entries").where({id}))
+    .then(entry => res.json(entry[0].entries))
 })
